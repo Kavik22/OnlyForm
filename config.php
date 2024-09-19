2bff63ab52953b7d3ceebbd699fe4d5213da161f
Dmitry Kraev <115352986+Kavik22@users.noreply.github.com>
2024-09-19 16:16:01 +0500

    Create config.php
    Add your keys for Yandex captcha to work

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_HOST', 'db');
+define('DB_PORT', '3306');
+define('DB_NAME', 'form');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('CLIENT_ID', 'YOUR_CLIENT_ID');
+define('SMARTCAPTCHA_SERVER_KEY', 'YOUR_SMARTCAPTCHA_SERVER_KEY');
